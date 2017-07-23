ec492a9319640c9e1a1439812ca9a97c8e768ef3
zhanyunzhi <540562332@qq.com>
2017-07-23 23:53:26 +0800

    07-02

---
+++ src/js/components/header.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ZYZ on 2017/7/23.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default class ComponentHeader extends React.Component{
+    render(){
+        return (
+            <header>
+                <h1>这里是头部</h1>
+            </header>
+        )
+    }
+}
